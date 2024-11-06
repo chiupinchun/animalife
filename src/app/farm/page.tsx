ae2dd44729251db7b23a5dea8d9ac27b83ef9406
Hato <fansintuesan@gmail.com>
2024-11-06 10:41:08 +0800

    revert: hide unfinished code to avoid tsc error

--- src/app/farm/page.tsx
+++ src/app/farm/page.tsx
@@ -1,36 +1,36 @@
-import { getRandomByRate } from '@app/common/utils/math'
+// import { getRandomByRate } from '@app/common/utils/math'
 import { Card, CardContent } from '@mui/material'
-import { FC, useState } from 'react'
-import { CROP_MUTATION_RATE } from './constants/farm'
+import { FC } from 'react'
+// import { CROP_MUTATION_RATE } from './constants/farm'
 
-class FarmPlot {
-  isLocked = true
-  isMutated: boolean
+// class FarmPlot {
+//   isLocked = true
+//   isMutated: boolean
 
-  constructor(
-    public seed: {
-      harvestTime: Date
-    }
-  ) {
-    this.isMutated = getRandomByRate(CROP_MUTATION_RATE)
-  }
-}
+//   constructor(
+//     public seed: {
+//       harvestTime: Date
+//     }
+//   ) {
+//     this.isMutated = getRandomByRate(CROP_MUTATION_RATE)
+//   }
+// }
 
 interface Props { }
 
 const Farm: FC<Props> = () => {
-  const [farmPlots, setFarmPlots] = useState<FarmPlot[]>([])
+  // const [farmPlots, setFarmPlots] = useState<FarmPlot[]>([])
 
   return (
     <>
       <div className='flex justify-center items-center w-screen h-screen'>
         <Card>
           <CardContent className='grid grid-cols-2 md:grid-cols-5 gap-5'>
-            {farmPlots.map((plot, index) => (
+            {/* {farmPlots.map((plot, index) => (
               <div key={index}>
                 地
               </div>
-            ))}
+            ))} */}
           </CardContent>
         </Card>
       </div>
